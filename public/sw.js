d98d3c44dec409f48798efb02995e47214dacb9a
Martin Parisi <martin.parisi@desarrollosnea.com>
2024-04-23 17:34:04 -0300

    sw

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,22 @@
-self.addEventListener('push', () => {
-    self.registration.showNotification('Hello world!', options);
+self.addEventListener('push', function(e)  {
+    var options={
+      body: "this notif",
+      icon: "images/example.png",
+      vibrate:[100,50,20],
+      data:{
+        dateOfArrival: Date.now(),
+        primateKeys: "2"
+
+      },
+      actions:[{
+        action: "explore",
+        title: "explore this new world",
+        icon: "images/example.png"
+      },
+      {action: "close", title:"Close", icon: "images/example.png"}
+      ]
+    };
+    e.waitUntil(self.registration.showNotification('Hello world!', options
+    ));
+  
   });
