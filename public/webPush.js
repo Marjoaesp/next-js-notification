102f7bc355cfac32de52a4d786df11d82c95583b
Martin Parisi <martin.parisi@desarrollosnea.com>
2024-04-24 10:39:07 -0300

    web push subscription

--- public/webPush.js
+++ public/webPush.js
@@ -15,6 +15,15 @@ webpush.setVapidDetails(
 
 
 // This is the same output of calling JSON.stringify on a PushSubscription
-const pushSubscription = {}
+const pushSubscription = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/d57RY8aRgXc:APA91bHkCVBDZx0swM-Vs9_63PGfjnYpr8FMh45Kmj-TW1mZ3RMSr20xeKHqBDJUIXtxamsbWrwxy3oJKDPOiDFywJvcCkGRZ5RFP7_eKpHHGK-MqT2VBtMtpZR7t6F75j4cNFZ8lwAA",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BOPd4nCXPg_elDZN1HfvqChMX7CaHuvy9V-K_hkChK3A1Xdi31xVXVD28FCz8bBH_SQKCwhTyAArqcHV8HIvSJU",
+    auth: "6B70ZLJ4HpoQE0SEr7oy0w",
+  },
+};
 
 webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
