4970f78ea53058ae7aa454907a5f14a162f99f17
Martin Parisi <martin.parisi@desarrollosnea.com>
2024-04-24 11:23:02 -0300

    webpush value

--- public/webPush.js
+++ public/webPush.js
@@ -17,12 +17,12 @@ webpush.setVapidDetails(
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
   endpoint:
-    "https://fcm.googleapis.com/fcm/send/dT4C7GB82qs:APA91bGsXZdsZoueFd7U9xHX0fcIb-0QA4v89_bqMKcnQqAhn5kNdTjULjlL1MJLp3LEIk1QSloxrxoIvbOP-j0__Ju5zWu9dTJPl9_cHo7wTI5mKOoHqcQa6w1NvhMerU8Yh3VG1CkQ",
+    "https://fcm.googleapis.com/fcm/send/etu2spplcbY:APA91bG7QHdoJ3laQNrtHj6Sbn24_2pm5KyTw8acfXKQ1lHJU8tT8903JD69UbdFfbXPNKJE9LiPlZ999V7l5PlDAwZCkOTAeLMImGl2Db2HVYZrM5Daeyvc9oORlrbTYV_4tY2N_Ckw",
   expirationTime: null,
   keys: {
     p256dh:
-      "BGNuuuxPLCuUe_oEg-de7tIYlsONL5TgaBwtsCq6OKyVc5Xqauc2p5jUmJ4h5zyR0bpJuSp_suwmvFpuv3UpECU",
-    auth: "RfwWw0UBKRG_Y_vQsrmxDw",
+      "BIeIA4DZvluwRvJr7OysRnP6NZvJurPEO0ofyZKpVla0mDTumF3xtnYlrOX8lKULY031FtKOn3SOe0GyMcmNRg0",
+    auth: "VT8jH7Pzc8HjMgp0IrCP_A",
   },
 };
 webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
